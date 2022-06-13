0ae56cfe200775f06dce753a0c905a038e182d5a
Nikita Antipin <nikprime1@gmail.com>
2022-06-13 18:14:16 +0300

    change route history to hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import BlogPage from "@/components/pages/BlogPage";
 import TechPage from "@/components/pages/TechPage";
 import MainPage from "@/components/pages/MainPage";
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 })
 
